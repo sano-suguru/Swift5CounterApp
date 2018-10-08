4999890b27db857db7a1f8922a4e6e06963b06b9
Sano Suguru <sano@mbp17.local>
2018-10-08 18:54:48 +0900

    Add plus/minus function

--- Swift5CountUp/ViewController.swift
+++ Swift5CountUp/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    @IBOutlet var countLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func plus(_ sender: Any) {
+        count = count + 1
+        countLabel.text = String(count)
+    }
 
+    @IBAction func minus(_ sender: Any) {
+        count = count  - 1
+        if (count < 0) {
+            count = 0
+        }
+        countLabel.text = String(count)
+    }
 }
-
